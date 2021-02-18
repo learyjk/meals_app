874340ec0488c83d53e1f5d07532bd21caa9f916
keeganleary <leary.keegan@gmail.com>
2021-02-18 08:01:57 -0800

    Initial commit

---
+++ lib/models/category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final id;
+  final String title;
+  final Color color;
+
+  const Category(
+      {@required this.id, @required this.title, this.color = Colors.orange});
+}
